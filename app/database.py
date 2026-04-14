093a1c15352a2422ee17e84570d76fb1903bbb6a
Marcial <MarcialGodes@gmail.com>
2026-04-14 19:44:15 +0200

    fix: remove local postgres config

--- app/database.py
+++ app/database.py
@@ -7,12 +7,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise Exception("❌ DATABASE_URL not set")
 
-print("DATABASE_URL:", DATABASE_URL)
+print("🔥 USING DATABASE:", DATABASE_URL)
 
-# 🔥 IMPORTANTE PARA RENDER
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,   # evita conexiones muertas
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
